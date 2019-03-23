3cbe987a4cf7c67f7cdae2dd503ded02512d7f79
Sam Trenholme <strenholme.usenet@gmail.com>
2019-03-23 03:05:34 -0700

    Fix case in generatePuzzles.sh to work on UNIX filesystem

--- Generator/generatePuzzles.sh
+++ Generator/generatePuzzles.sh
@@ -5,9 +5,9 @@ mkdir Puzzles/
 
 for TEMPLATE in 1 1 2 3 4 5  ; do
 	for iteration in 1 2 3 ; do
-		cp Prosperity-7-Template-${TEMPLATE}.xml in.xml
+		cp Prosperity-7-template-${TEMPLATE}.xml in.xml
 		rm -f out.xml
-		Java -jar SudokuGenerate.jar > /dev/null 2>&1
+		java -jar SudokuGenerate.jar > /dev/null 2>&1
 		mv out.xml Puzzles/$( cat out.xml | \
 			cut -f8 -d\" | tr -d ' ' | \
 			tr -d '\r' | tr -d '\n')-$( date +%s ).xml
